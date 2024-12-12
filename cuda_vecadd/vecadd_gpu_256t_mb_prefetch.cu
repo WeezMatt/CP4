878178cd0b89ca549526fbd6fbf81d33fde14d2f
Matthew Aaron Weesner <weezmatt@x1000c3s3b0n1h0.chn.perlmutter.nersc.gov>
2024-12-11 21:10:24 -0800

    Initial commit: Added cuda_vecadd folder

---
+++ cuda_vecadd/vecadd_gpu_256t_mb_prefetch.cu
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+#include <chrono>
+// Kernel function to add the elements of two arrays
+__global__
+void add(int n, float *x, float *y)
+{
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride)
+    y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+  int deviceID=0; 
+  int N = 1<<20;
+  float *x, *y;
+  int blockSize = 256;
+  int numBlocks = (N + blockSize - 1) / blockSize;
+  // Allocate Unified Memory – accessible from CPU or GPU
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+
+  cudaMemPrefetchAsync((void *)x, N*sizeof(float), deviceID) ;
+  cudaMemPrefetchAsync((void *)y, N*sizeof(float), deviceID) ;
+  // as per google doc print out number of threads 
+  std::cout << "numblocks: " << numBlocks << " blocks: " << blockSize << " threads per:" << std::endl;
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+  // Run kernel on 1M elements on the GPU
+  add<<<numBlocks, blockSize>>>(N, x, y);
+
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  cudaFree(x);
+  cudaFree(y);
+
+  return 0;
+}
